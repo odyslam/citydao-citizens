fc1c0f2304cc74cc4159e01ec4ba0eecd72b31cd
Odysseas Lamtzidis <odyslam@gmail.com>
2021-10-12 21:17:53 +0300

    feat: Prepare scripts for deployment

--- scripts/test-deploy.sh
+++ scripts/test-deploy.sh
@@ -9,7 +9,7 @@ set -eo pipefail
 . $(dirname $0)/deploy.sh
 
 # get the address
-addr=$(jq -r '.Greeter' out/addresses.json)
+addr=$(jq -r '.CitizenNFT' out/addresses.json)
 
 # the initial greeting must be empty
 greeting=$(seth call $addr 'greeting()(string)')
