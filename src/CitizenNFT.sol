ef0515de6086c42c96a8a6ece38f4543be5fe20c
Odysseas Lamtzidis <odyslam@gmail.com>
2021-10-24 12:40:26 +0100

    feat: erc1155 conversion

--- src/CitizenNFT.sol
+++ src/CitizenNFT.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.7;
 
 // ============ Imports ============
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
@@ -25,17 +25,16 @@ interface FrackingClosedSourceContract {
 /// @notice An ERC721 NFT that replaces the Citizen NFTs that are issued by the OpenSea Storefront Smart contract.
 /// This smart contract enables users to either mint a new CitizenNFT or
 /// "transfer" their Citizen NFTs from the OpenSea smart contract to this one.
-contract CitizenNFT is ERC721, Ownable, DSTest {
+contract CitizenNFT is ERC1155, Ownable, DSTest {
     // We use safemath to avoid under and over flows
     using SafeMath for uint256;
-    // The maximum number of NFTs that can be minted for every category: Regular, Founding, First
-    // These variables can be changed by the appropriate function
+
     uint256 private availableHousingForCitizens = 10000;
     uint256 private museumSize = 50;
     uint256 private constant firstCitizensAmongstEquals = 1;
     // At the time of writing, a new CitizenNFT costs 0.25 ether.
     // This variable can change by the appropriate function
-    uint256 public citizenshipStampCostInWei = 250000000000000000;
+    uint256 private citizenshipStampCostInWei = 250000000000000000;
     // Variable that stores the address of the deployed  OpenSea shared Storefront smart contract
     // At the time of writing,
     // this is the address: https://etherscan.io/address/0x495f947276749ce646f68ac8c248420045cb7b5e
@@ -63,24 +62,19 @@ contract CitizenNFT is ERC721, Ownable, DSTest {
     // Map the interal Ids we use for the different types of citizen IDs to the ones used
     // in the Open Sea shared Storefront
     mapping(uint256 => uint256) private citizenNFTtoOpenSea;
-    mapping(uint256 => uint256) private citizenIdtoType;
-
     // Count how many different Citizen NFTs exist by type
     uint256 private citizenCounter;
     uint256 private foundingCitizenCounter;
-    uint256 private firstCitizenCounter;
     // The ID of every Citizen NFT. Every Citizen NFT (regular, founding, first) belongs to the same NFT,
     // but has different metadata. Thus there is a global ID for every citizen NFT.
-    uint256 private universalCitizenId;
-
     // Events
     event LogEthDeposit(address);
     event CitizenLegislatureChanged(string, uint256);
     event RefugeeSaved(address, uint256);
     event NewCitizen(address, uint256, uint256);
-
+    // erc1155
+    mapping(uint256 => uint256) mintedNFTs;
     FrackingClosedSourceContract private frackingClosedSourceContract;
-
     /// @notice Initialise CitizenNFT smart contract with the appropriate address and ItemIds of the
     /// Open Sea shared storefront smart contract and the Citizen NFTs that are locked in it.
     constructor(
@@ -88,7 +82,7 @@ contract CitizenNFT is ERC721, Ownable, DSTest {
         uint256 _jailedCitizens,
         uint256 _citizensFacingGuillotine,
         uint256 _beheadedCitizen
-    ) Ownable() ERC721("CityDAO Citizen", "CTZN") {
+    ) Ownable() ERC1155("") {
         frackingClosedSourceContract = FrackingClosedSourceContract(
             _forbiddenAddress
         );
@@ -96,30 +90,37 @@ contract CitizenNFT is ERC721, Ownable, DSTest {
         citizenNFTtoOpenSea[FOUNDING_NFT_ID] = _citizensFacingGuillotine;
         citizenNFTtoOpenSea[FIRST_NFT_ID] = _beheadedCitizen;
         forbiddenAddress = _forbiddenAddress;
+        citizenCounter = availableHousingForCitizens;
+        foundingCitizenCounter = museumSize;
     }
-
+    function initialCitizenship() external onlyOwner {
+        _mint(msg.sender, CITIZEN_NFT_ID, availableHousingForCitizens,"");
+        _mint(msg.sender, FOUNDING_NFT_ID, museumSize,"");
+        _mint(msg.sender, FIRST_NFT_ID, firstCitizensAmongstEquals,"");
+        setApprovalForAll(address(this), true);
+    }
     /// @notice Change the cost for minting a new regular Citizen NFT
     /// Can only be called by the owner of the smart contract.
     function legislateCostOfEntry(uint256 _stampCost) external onlyOwner {
         citizenshipStampCostInWei = _stampCost;
         emit CitizenLegislatureChanged("stampCost", _stampCost);
     }
-
-    /// @notice Change the maximum number of regular Citizen NFTs that can be minted
+    /// @notice Mint new Citizen NFTs to the owner of the smart contract
     /// Can only be called by the owner of the smart contract.
-    function legislateForHousing(uint256 _newMaxCitizenNFTs)
+    function buildHousing(uint256 _newCitizens)
         external
         onlyOwner
     {
-        availableHousingForCitizens = _newMaxCitizenNFTs;
-        emit CitizenLegislatureChanged("citizenNftMax", _newMaxCitizenNFTs);
+        _mint(msg.sender, CITIZEN_NFT_ID, _newCitizens,"");
+        foundingCitizenCounter = foundingCitizenCounter.add(_newCitizens);
+        emit CitizenLegislatureChanged("Minted new citizen NFTs", _newCitizens);
     }
-
-    /// @notice Change the maximum number of Founding Citizen NFTs that can be minted
+    /// @notice  Mint new Citizen NFTs to the owner of the smart contract
     /// Can only be called by the owner of the smart contract.
-    function rewriteHistory(uint256 _max) external onlyOwner {
-        museumSize = _max;
-        emit CitizenLegislatureChanged("foundingCitizenNftMax", _max);
+    function rewriteHistory(uint256 _newCitizens) external onlyOwner {
+        _mint(msg.sender, FOUNDING_NFT_ID, _newCitizens,"");
+        foundingCitizenCounter = foundingCitizenCounter.add(_newCitizens);
+        emit CitizenLegislatureChanged("Minted new Founding Citizen NFTs", _newCitizens);
     }
 
     /// @notice Return the current cost of minting a new regular Citizen NFT.
@@ -127,18 +128,18 @@ contract CitizenNFT is ERC721, Ownable, DSTest {
         return citizenshipStampCostInWei;
     }
 
-    /// @notice Return the current maximum number of regular Citizen NFTs.
+    /// @notice Return the number of minted Citizen NFTs
     function inquireHousingNumbers() external view returns (uint256) {
-        return availableHousingForCitizens;
+        return mintedNFTs[CITIZEN_NFT_ID];
     }
 
-    /// @notice Return the current maximum number of Founding NFTs.
+    /// @notice Return the current maximum number of  minted Founding Citizen NFTs
     function inquireAboutHistory() external view returns (uint256) {
-        return museumSize;
+        return mintedNFTs[FOUNDING_NFT_ID];
     }
 
-    /// @notice Return the number of NFTs  of a particular type that the address has locked
-    /// in the Open Sea shared Storefront smart contract.
+    /// @notice Return the number of NFTs  of a particular type that the address
+    /// already owns in the OpenSea shared storefront
     function inquireRefugeeStatus(
         address _refugeeAddress,
         uint256 _citizensType
@@ -149,39 +150,27 @@ contract CitizenNFT is ERC721, Ownable, DSTest {
                 citizenNFTtoOpenSea[_citizensType]
             );
     }
-
-    /// @notice Mint a new citizen NFT
-    function onlineApplicationForCitizenship()
+    /// @notice Transfer regular Citizen NFTs from the CityDAO owner address to the user.
+    /// @param _citizenNumber How many citizenNFTs will be transfered.
+    /// The user must include in the transaction, the appropriate number of ether in Wei.
+    function onlineApplicationForCitizenship(uint256 _citizenNumber)
         public
         payable
-        returns (uint256)
     {
         require(
-            msg.value >= citizenshipStampCostInWei,
+            msg.value >= citizenshipStampCostInWei * _citizenNumber,
             "ser, the state machine needs oil"
         );
-        return issueCitizenship(msg.sender, CITIZEN_NFT_ID);
+        _safeTransferFrom(this.owner(), msg.sender, CITIZEN_NFT_ID, _citizenNumber,"");
     }
-
-    /// @notice Mint a  new citizen NFT, only at working hours
-    function bureauApplicationForCitizenship() external payable {
-        uint256 day = (block.timestamp / 86400 + 3) % 7;
-        uint256 hour = (block.timestamp / 3600) % 24;
-        require(
-            day < 5 && hour >= 14 && hour < 21,
-            "this contract is only active monday through friday 10am to 5pm eastern time"
-        );
-        onlineApplicationForCitizenship();
-    }
-
-    /// @notice Withdraw the funds from the minting of new regular Citizen NFTs.
+    /// @notice Withdraw the funds locked in the smart contract,
+    /// originating from the minting of new regular Citizen NFTs.
     /// Can only be called by the owner of the smart contract.
     function raidTheCoffers() external onlyOwner {
         uint256 amount = address(this).balance;
         (bool success, ) = owner().call{value: amount}("");
         require(success, "Anti-corruption agencies stopped the transfer");
     }
-
     fallback() external payable {
         emit LogEthDeposit(msg.sender);
     }
@@ -189,16 +178,16 @@ contract CitizenNFT is ERC721, Ownable, DSTest {
     receive() external payable {
         emit LogEthDeposit(msg.sender);
     }
-
-    /// @notice Mint a citizen NFT that already exists locked in the Open Sea shared Storefront smart contract.
-    /// It returns the ID of the minted CityDAO NFT.
+    /// @notice Transfers Citizen NFTs from the CityDAO treasury to a user that already owns Citizen NFTs
+    /// in the OpenSea shared storefront smart contract.
     /// @param _refugeeAddress The address which owns the locked NFT and to which the new NFT will be minted to
     /// @param _citizenType The type of Citizen NFT. Regular: 42, Founding: 69, First: 7.
     /// They are defined as constants at the start of the smart contract.
     function applyForRefugeeStatus(
         address _refugeeAddress,
-        uint256 _citizenType
-    ) public returns (uint256 refugeeId) {
+        uint256 _citizenType,
+        uint256 _refugeeNumber
+    ) public  {
         uint256 tempCitizen;
 
         if (_citizenType == CITIZEN_NFT_ID) {
@@ -218,7 +207,7 @@ contract CitizenNFT is ERC721, Ownable, DSTest {
                 citizenNFTtoOpenSea[_citizenType]
             );
         }
-        tempCitizen = tempCitizen.sub(1);
+        tempCitizen = tempCitizen.sub(_refugeeNumber);
         if (tempCitizen == 0) {
             tempCitizen = 6969696969;
         }
@@ -231,65 +220,26 @@ contract CitizenNFT is ERC721, Ownable, DSTest {
         }
         citizens[_refugeeAddress] = tempCitizen;
         emit RefugeeSaved(_refugeeAddress, _citizenType);
-        return issueCitizenship(_refugeeAddress, _citizenType);
+        _safeTransferFrom(this.owner(), _refugeeAddress, _citizenType, _refugeeNumber,"");
     }
-
-    /// @notice Return the type of a particular Citizen NFT, based on it's id.
-    function citizenshipVerification(uint256 _citizenshipId)
-        external
-        view
-        returns (uint256)
-    {
-        return citizenIdtoType[_citizenshipId];
-    }
-
-    /// @notice Internal function that mints a new citizen NFT. It chekcs whether the maximum number of cityDAO nfts
-    /// has been minted for every type of CityDAO NFT (regular, founding, first).
-    function issueCitizenship(address _citizenAddress, uint256 _citizenType)
-        private
-        returns (uint256 citizenNFTId)
-    {
-        if (_citizenType == CITIZEN_NFT_ID) {
-            require(
-                citizenCounter <= availableHousingForCitizens,
-                "No more permits are issued"
-            );
-            citizenCounter = citizenCounter.add(1);
-        } else if (_citizenType == FOUNDING_NFT_ID) {
-            require(
-                foundingCitizenCounter <= museumSize,
-                "No more permits are issued"
-            );
-            foundingCitizenCounter = foundingCitizenCounter.add(1);
-        } else if (_citizenType == FIRST_NFT_ID) {
-            require(firstCitizenCounter == 0, "No more permits are issued");
-            firstCitizenCounter = firstCitizenCounter.add(1);
-        }
-        citizenIdtoType[universalCitizenId] = _citizenType;
-        universalCitizenId = universalCitizenId.add(1);
-        emit NewCitizen(_citizenAddress, universalCitizenId - 1, _citizenType);
-        _safeMint(_citizenAddress, universalCitizenId - 1);
-        return universalCitizenId - 1;
-    }
-
-    /// @notice Returns the metadata for a particular CityDAo Citizen NFT, based on it's ID.
+    /// @notice Returns the metadata for a CitizenNFT, based on it's ID.
     /// Every ID is mapped to a Citizen NFT type (Regular: 42, Founding: 68, First: 7). Based on the mapping
-    /// the citizen NFT has different metadata. Apart from the metadata, all Citizen NFTs are the same NFT.
-    function tokenURI(uint256 _citizenshipId)
+    /// the citizen NFT has different metadata.
+    function uri(uint256 _citizenshipId)
         public
-        view
+        pure
         override
         returns (string memory)
     {
         string memory imageHash;
         string memory citizenship;
-        if (citizenIdtoType[_citizenshipId] == 42) {
+        if ( _citizenshipId== 42) {
             imageHash = "QmRRnuHVwhoYEHsTxzMcGdrCfthKTS65gnfUqDZkv6kbza";
             citizenship = "CityDAO Citizen";
-        } else if (citizenIdtoType[_citizenshipId] == 69) {
+        } else if ( _citizenshipId == 69) {
             imageHash = "QmSrKL6fhPYU6BbYrV97AJm3aM6naGWZK95QntXXZuGQrF";
             citizenship = "CityDAO Founding Citizen";
-        } else if (citizenIdtoType[_citizenshipId] == 7) {
+        } else if ( _citizenshipId == 7) {
             imageHash = "Qmb6VmYiktfvNX3YkLosYwjUM82PcEkr2irZ4PWheYiG2b";
             citizenship = "CityDAO First Citizen";
         } else {
